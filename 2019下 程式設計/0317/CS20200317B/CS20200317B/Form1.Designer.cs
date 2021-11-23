756b499dccd4b357af0826cfbbc2c7e11524f994
zxc0319 <74039522+zxc0319@users.noreply.github.com>
2021-11-23 18:38:35 +0800

    Add files via upload

---
+++ 2019下 程式設計/0317/CS20200317B/CS20200317B/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CS20200317B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Display = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Switch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Display
+            // 
+            this.Display.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Display.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Display.ForeColor = System.Drawing.Color.Blue;
+            this.Display.Location = new System.Drawing.Point(81, 60);
+            this.Display.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Display.Name = "Display";
+            this.Display.ReadOnly = true;
+            this.Display.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Display.Size = new System.Drawing.Size(593, 33);
+            this.Display.TabIndex = 0;
+            this.Display.Text = "資管一乙";
+            this.Display.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Display.DoubleClick += new System.EventHandler(this.Display_DoubleClick);
+            this.Display.MouseEnter += new System.EventHandler(this.Display_MouseEnter);
+            this.Display.MouseLeave += new System.EventHandler(this.Display_MouseLeave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(232, 339);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 30);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(232, 204);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 30);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Switch
+            // 
+            this.Switch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Switch.Location = new System.Drawing.Point(693, 60);
+            this.Switch.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Switch.Name = "Switch";
+            this.Switch.Size = new System.Drawing.Size(89, 33);
+            this.Switch.TabIndex = 3;
+            this.Switch.Text = "on/off";
+            this.Switch.UseVisualStyleBackColor = false;
+            this.Switch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(897, 713);
+            this.Controls.Add(this.Switch);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Display);
+            this.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Display;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Switch;
+    }
+}
+
---
+++ 2019下 程式設計/0317/CS20200317B/CS20200317B/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS20200317B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Display.Text = DateTime.Now.ToString();
+        }
+
+        private void Display_DoubleClick(object sender, EventArgs e)
+        {
+            Display.Text = DateTime.Now.ToString();
+        }
+
+        private void Display_MouseEnter(object sender, EventArgs e)
+        {
+            Display.Text = DateTime.Now.ToString();
+        }
+
+        private void Display_MouseLeave(object sender, EventArgs e)
+        {
+            Display.Text ="資管一乙";            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Display.Text = DateTime.Now.ToString();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = ! timer1.Enabled;
+            if (timer1.Enabled)
+            {
+                Switch.Text = "OFF";
+                Switch.BackColor = Color.Green;
+            }
+            else
+            {
+                Switch.Text = "ON";
+                Switch.BackColor = Color.Red;
+            }
+        }
+    }
+}
